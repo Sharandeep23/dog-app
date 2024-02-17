f84bca8c8091ca5f53996a5022dc2f974f7ecaa2
Sharandeep <sharandeep23@yahoo.com>
2024-02-17 17:48:59 +0600

    new approach

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 const main = document.querySelector('main');
-const breedSelect = document.querySelector('.breed-list');
-const breedImg = document.querySelector('.main-image');
+const breedSelect = document.querySelector('.breed');
+const img = document.querySelector('.main-image');
 const loader = document.querySelector('.loading-dog');
 
 const BREED_LIST_URL = 'https://dog.ceo/api/breeds/list/all';
-const RANDOM_BREED_IMG_URL = 'https://dog.ceo/api/breeds/image/random';
 
 function init() {
   // Breed list for the select
-  addBreedList(BREED_LIST_URL);
-
-  // Without any selection, a random img will be shown
-  addRandomImg(RANDOM_BREED_IMG_URL);
+  populateSelect(BREED_LIST_URL);
 
   // When a breed is selected, hide the img, show the spinner, get the breed img, add it to the DOM.
   breedSelect.addEventListener('change', handleBreedChange);
 
   // When Image loads, show the image and hide the spinner/loader
-  breedImg.addEventListener('load', handleBreedImgLoad);
+  img.addEventListener('load', handleImgLoad);
 }
 
-async function addBreedList(url) {
+async function populateSelect(url) {
   try {
     const response = await axios.get(url);
     const breedList = Object.keys(response.data.message);
@@ -40,28 +36,16 @@ async function addBreedList(url) {
   }
 }
 
-async function addRandomImg(url) {
-  try {
-    const response = await axios.get(url);
-    const randBreedImgUrl = response.data.message;
-
-    // This line doesn't add the image instantly
-    breedImg.src = randBreedImgUrl;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 async function handleBreedChange(e) {
   const breed = e.target.value;
 
-  // For empty strings
+  // For empty strings, ignore
   if (breed === '') return;
 
   const IMG_BY_BREED_URL = `https://dog.ceo/api/breed/${breed}/images/random`;
 
   // changing option should hide image and show the spinner/loader
-  breedImg.classList.remove('show');
+  img.classList.remove('show');
   loader.classList.add('show');
 
   try {
@@ -69,15 +53,15 @@ async function handleBreedChange(e) {
     const breedImgUrl = response.data.message;
 
     // This line doesn't add the image instantly
-    breedImg.src = breedImgUrl;
+    img.src = breedImgUrl;
   } catch (error) {
     console.error(error);
   }
 }
 
-function handleBreedImgLoad() {
+function handleImgLoad() {
   // adding image and removing loader
-  breedImg.classList.add('show');
+  img.classList.add('show');
   loader.classList.remove('show');
 }
 
